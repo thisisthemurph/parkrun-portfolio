f0c0761c1d2510ab01a3f9e4b6401ffbc538e792
= <michael.w.murphy@outlook.com>
2020-04-02 14:14:25 +0100

    Updated the app to present the overview statistics for the user

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Search from '../Components/Search'
+import Portfolio from '../Components/Portfolio'
 
-// import './App.css';
+import './App.scss';
 
 class App extends Component {
   constructor() {
@@ -9,6 +10,7 @@ class App extends Component {
     this.state = {
       user: {},
       userDoesNotExist: false,
+      loading: false,
       searchText: ''
     }
   }
@@ -25,7 +27,13 @@ class App extends Component {
 
   onSearchButtonClick = async (event) => {
     console.log('Fetching data...')
+
+    this.setState({ loading: true })
+
     const res = await fetch(`http://localhost:5000/parkrun/api/${this.state.searchText}`)
+
+    console.log('Data is got')
+    this.setState({ loading: false })
     
     if (res.ok) {
       const user = await res.json()
@@ -35,6 +43,9 @@ class App extends Component {
       if (res.status === 404) {
         console.log('the user does not exist')
         this.setState({ userDoesNotExist: true })
+      } else {
+        console.log('some other error')
+        console.error(res)
       }
     }
   }
@@ -46,6 +57,10 @@ class App extends Component {
         <Search 
           searchChange={this.onSearchChange}
           searchButtonClick={this.onSearchButtonClick}
+        />
+        <Portfolio
+          user={this.state.user}
+          loading={this.state.loading}
         />
       </div>
     );
