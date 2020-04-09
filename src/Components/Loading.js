95b3c7dac8e0cfe6a94c83fbd5f549bd4d1c2e39
= <michael.w.murphy@outlook.com>
2020-04-09 16:35:59 +0100

    Loading component now displays a random gif

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,10 +1,33 @@
 import React from 'react';
-// import './Loading.css';
+import './Loading.scss';
+
+const loadingGifs = [
+  "https://giphy.com/embed/l2Sqc3POpzkj5r8SQ",
+  "https://giphy.com/embed/CUHXyh3yXr9kI",
+  "https://giphy.com/embed/HNJHYpKqtuqRy",
+  "https://giphy.com/embed/5lLQ4I8vQYxJm",
+  "https://giphy.com/embed/2y77qqREIzrQk",
+  "https://giphy.com/embed/NRkK2XCXPd2Du",
+  "https://giphy.com/gifs/zebra-yOZ5hsdLjAp8Y",
+  "https://giphy.com/embed/3oEjHW0rhhhVopXFVm",
+  "https://giphy.com/embed/l0MYyrLWpUM3YQSqs"
+]
 
 function Loading() {
   return (
-    <p className='Loading'>Loading...</p>
+    <div className='Loading'>
+      <p>Just <span>running</span> that for you...</p>
+      <div className='Loading-gif'>
+        {getRandomGif()}
+      </div>
+    </div>
   );
+}
+
+const getRandomGif = () => {
+  const r = Math.floor(Math.random() * loadingGifs.length)
+  const url = loadingGifs[r]
+  return <iframe title='Loading' src={url} width="100%" height="100%" frameBorder="0" allowFullScreen></iframe>
 }
 
 export default Loading;
